20a842e2f1330fcec0b3cd17bffdb8bc3b2d3d2f
Ville Peurala <ville.peurala@gmail.com>
2017-03-19 10:20:47 +0200

    Assertions for buildings B 44 and B 45

--- src/test/java/com/shipyard/application/repository/jdbc/JdbcBuildingRepositoryTest.java
+++ src/test/java/com/shipyard/application/repository/jdbc/JdbcBuildingRepositoryTest.java
@@ -46,6 +46,12 @@ public class JdbcBuildingRepositoryTest {
         Building building1 = buildings.get(0);
         assertEquals("B 43", building1.getCode());
         assertEquals("Assembly shed", building1.getDescription());
+        Building building2 = buildings.get(1);
+        assertEquals("B 44", building2.getCode());
+        assertEquals("Factory", building2.getDescription());
+        Building building3 = buildings.get(2);
+        assertEquals("B 45", building3.getCode());
+        assertEquals("Paint shop", building3.getDescription());
     }
 
     private String stringResource(String name) throws IOException {
